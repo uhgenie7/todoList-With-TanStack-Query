8a31b174046ee6810e4f1f7e0c9884acb017b2d0
uhj1993 <dzzd777@naver.com>
2022-08-09 13:57:20 +0900

    feat: Label component

--- src/components/atoms/Label/index.tsx
+++ src/components/atoms/Label/index.tsx
@@ -1,5 +1,15 @@
+import styled from 'styled-components';
+
 const Label = ({ labelFor, labelValue }: { labelFor: string; labelValue: string }) => {
-  return <label htmlFor={labelFor}>{labelValue}: </label>;
+  return <Container htmlFor={labelFor}>{labelValue}</Container>;
 };
 
 export default Label;
+
+const Container = styled.label`
+  display: inline-block;
+  width: 70px;
+  font-size: 0.8rem;
+  margin-right: 0.5rem;
+  outline: none;
+`;
