450151b93466104459ef080221c2484b6d10a738
uhj1993 <dzzd777@naver.com>
2022-08-09 19:12:59 +0900

    feat: react query 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GlobalStyle from '@src/styles/globalStyle';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(new QueryClient());
   return (
-    <>
-      <ToastContainer />
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ToastContainer />
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
