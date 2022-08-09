a2ee16efc94c59e0a44244a040cd5f0dba1be551
uhj1993 <dzzd777@naver.com>
2022-08-09 19:12:20 +0900

    fix: 컴포넌트 이름 수정

--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -1,4 +1,4 @@
-import Form from '@src/components/organism/Form';
+import AuthForm from '@src/components/organism/AuthForm';
 import { signUpAPI } from '@src/apis/auth';
 import AppLayout from '@src/components/atoms/AppLayout';
 
@@ -6,7 +6,7 @@ const SignUp = () => {
   return (
     <AppLayout>
       <h2>회원가입</h2>
-      <Form buttonValue="회원가입" handleAPI={signUpAPI} />
+      <AuthForm buttonValue="회원가입" handleAPI={signUpAPI} />
     </AppLayout>
   );
 };
