d8cc37b084e178c227ab47176457d19aa6a1febe
uhj1993 <dzzd777@naver.com>
2022-08-09 19:12:31 +0900

    fix: 컴포넌트 이름 수정

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import Button from '@src/components/atoms/Button';
-import Form from '@src/components/organism/Form';
+import AuthForm from '@src/components/organism/AuthForm';
 import { useRouter } from 'next/router';
 import { loginAPI } from '@src/apis/auth';
 import styled from 'styled-components';
@@ -20,7 +20,7 @@ const Auth = () => {
     <Container>
       <h2>로그인</h2>
       <div className="center">
-        <Form buttonValue="로그인" handleAPI={loginAPI} />
+        <AuthForm buttonValue="로그인" handleAPI={loginAPI} />
         <Button isCorrect={true} buttonValue="회원가입" onClick={handleNavigateSignUp} />
       </div>
     </Container>
