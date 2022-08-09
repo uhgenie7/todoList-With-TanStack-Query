845f36be7ce89aeacdd4c2b8a3d443f2d4a93d84
uhj1993 <dzzd777@naver.com>
2022-08-09 13:57:33 +0900

    feat: InputForm component

--- src/components/molecules/InputForm.tsx
+++ src/components/molecules/InputForm.tsx
@@ -1,6 +1,7 @@
 import { InputHTMLAttributes } from 'react';
 import Label from '@src/components/atoms/Label';
 import Input from '@src/components/atoms/Input';
+import styled from 'styled-components';
 
 interface IProps extends InputHTMLAttributes<HTMLInputElement> {
   labelFor: string;
@@ -12,12 +13,20 @@ interface IProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const InputForm = ({ labelFor, labelValue, errorMessage, isError, ...props }: IProps) => {
   return (
-    <>
+    <Container>
       <Label labelFor={labelFor} labelValue={String(labelValue)} />
       <Input labelFor={labelFor} {...props} />
-      {isError && <p>{errorMessage}</p>}
-    </>
+      {isError && <p>! {errorMessage}</p>}
+    </Container>
   );
 };
 
 export default InputForm;
+
+const Container = styled.div`
+  p {
+    margin-top: 0.5rem;
+    font-size: 0.8rem;
+    color: var(--danger);
+  }
+`;
