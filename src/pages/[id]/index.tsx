90a7299318f25f176349f7ef52b0d14d39befe6b
uhj1993 <dzzd777@naver.com>
2022-08-09 19:13:07 +0900

    feat: 다이나믹 라우팅

---
+++ src/pages/[id]/index.tsx
@@ -0,0 +1,12 @@
+import Todos from '@src/components/organism/Todos';
+import AppLayout from '@src/components/atoms/AppLayout';
+
+const TodoDetail = () => {
+  return (
+    <AppLayout>
+      <Todos />
+    </AppLayout>
+  );
+};
+
+export default TodoDetail;
