529a05f3e1ecb9fe4120433b08adb5ba93465caf
HyejinUm <uhj1993@naver.com>
2022-08-10 19:25:52 +0900

    Type: (#4) SSP Type 정의
    - axios와 엮인 API를 비구조화 할당 했을 때, 타입이 잡히지 않음...

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import showToast from '@src/libs/common';
@@ -8,7 +9,7 @@ import { getTodosAPI } from '@src/apis/todos';
 import { ITodoArr } from '@src/types';
 import { Suspense } from 'react';
 
-const Home = ({ todos }: ITodoArr) => {
+const Home = ({ todos = [] }: ITodoArr) => {
   const toast = showToast();
   const router = useRouter();
   useEffect(() => {
@@ -35,10 +36,10 @@ const Home = ({ todos }: ITodoArr) => {
 
 export default Home;
 
-export const getServerSideProps = async () => {
-  const {
-    data: { data: todos },
-  } = await getTodosAPI();
+export const getServerSideProps: GetServerSideProps = async () => {
+  // const { data: todos } = await getTodosAPI(); 타입 정의가 너무 어려움
+  const data = await getTodosAPI();
+  const todos = data?.data;
 
   return {
     props: { todos },
