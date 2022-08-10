dcab56c15fc1e2b1ae77085a53377aa1e3142b12
HyejinUm <uhj1993@naver.com>
2022-08-10 19:14:06 +0900

    fix: (#8) status === 200을 삭제

--- client/src/apis/auth/index.ts
+++ client/src/apis/auth/index.ts
@@ -1,30 +1,23 @@
 import { instance } from '..';
-import showToast from '@src/libs/common';
-const toast = showToast();
 
 export const signUpAPI = async (data: any) => {
   try {
     const response = await instance.post('/users/create', data);
-    if (response.status === 200) {
-      toast.success(response.data.message);
-    }
-
+    localStorage.setItem('token', response.token);
     console.log(response);
     return response;
   } catch (error) {
-    console.log(error);
+    return error;
   }
 };
 
 export const loginAPI = async (data: any) => {
   try {
     const response = await instance.post('/users/login', data);
-    if (response.status === 200) {
-      toast.success(response.data.message);
-      localStorage.setItem('token', response.data.token);
-    }
+    console.log(response);
+    localStorage.setItem('token', response.token);
     return response;
   } catch (error) {
-    console.log(error);
+    return error;
   }
 };
