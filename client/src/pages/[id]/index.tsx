f8084c765d0e31723adae2239fc2ef41466e4376
HyejinUm <uhj1993@naver.com>
2022-08-11 16:55:45 +0900

    fix: (#8) AppLayout 변경

--- client/src/pages/[id]/index.tsx
+++ client/src/pages/[id]/index.tsx
@@ -1,13 +1,12 @@
 import { useRouter } from 'next/router';
 import Todos from '@src/components/organism/Todos';
-import AppLayout from '@src/components/atoms/AppLayout';
 
 const TodoDetail = () => {
   const {
     query: { id },
   } = useRouter();
 
-  return <AppLayout>{id && <Todos />}</AppLayout>;
+  return <>{id && <Todos />}</>;
 };
 
 export default TodoDetail;
--- client/src/pages/auth/signup/index.tsx
+++ client/src/pages/auth/signup/index.tsx
@@ -1,13 +1,17 @@
 import AuthForm from '@src/components/organism/AuthForm';
 import { signUpAPI } from '@src/apis/auth';
 import AppLayout from '@src/components/atoms/AppLayout';
+import { useUserActions } from '@src/utils/user';
 
 const SignUp = () => {
+  const userActions = useUserActions();
+
   return (
-    <AppLayout>
+    <>
       <h2>회원가입</h2>
       <AuthForm buttonValue="회원가입" handleLoginAPI={signUpAPI} />
-    </AppLayout>
+      {/* <AuthForm buttonValue="회원가입" handleLoginAPI={() => userActions.handleSignUp} /> */}
+    </>
   );
 };
 
