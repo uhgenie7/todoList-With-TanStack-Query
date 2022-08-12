d6b5865cb6e91c0986f655472a1a09da70663266
uhj1993 <dzzd777@naver.com>
2022-08-12 16:20:54 +0900

    fix: (#8) page인 signup을 개선

--- client/src/pages/auth/signup/index.tsx
+++ client/src/pages/auth/signup/index.tsx
@@ -1,16 +1,11 @@
 import AuthForm from '@src/components/organism/AuthForm';
 import { signUpAPI } from '@src/apis/auth';
-import AppLayout from '@src/components/atoms/AppLayout';
-import { useUserActions } from '@src/utils/user';
 
 const SignUp = () => {
-  const userActions = useUserActions();
-
   return (
     <>
       <h2>회원가입</h2>
       <AuthForm buttonValue="회원가입" handleLoginAPI={signUpAPI} />
-      {/* <AuthForm buttonValue="회원가입" handleLoginAPI={() => userActions.handleSignUp} /> */}
     </>
   );
 };
