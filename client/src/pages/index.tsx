2a5084cd8a1fd1a2dbdb847da3f7dc6c0fdcaa57
uhj1993 <dzzd777@naver.com>
2022-08-12 16:20:28 +0900

    fix: (#8) page인 home을 개선

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,34 +1,20 @@
-import { useEffect } from 'react';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
-import { useRecoilValue } from 'recoil';
-import { isLoginState } from '@src/states/loginState';
-import { useRouter } from 'next/router';
-
-import AuthLayout from '@src/components/atoms/AuthLayout';
 import Todos from '@src/components/organism/Todos';
 import { getTodosAPI } from '@src/apis/todos';
 import { ITodoArr } from '@src/types';
 import { Suspense } from 'react';
 
 const Home = ({ todos = [] }: ITodoArr) => {
-  const isLoggedIn = useRecoilValue(isLoginState);
-
   return (
     <div>
       <Head>
         <title>wanted-pre-onboarding-challenge-fe-1</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <AuthLayout>
-        {isLoggedIn ? (
-          <Suspense fallback={<div>loading</div>}>
-            <Todos todos={todos} />
-          </Suspense>
-        ) : (
-          <>로그인</>
-        )}
-      </AuthLayout>
+      <Suspense fallback={<div>loading</div>}>
+        <Todos todos={todos} />
+      </Suspense>
     </div>
   );
 };
