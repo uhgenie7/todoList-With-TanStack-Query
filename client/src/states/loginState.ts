65014440960e5f9f0495876aea0f85cec4870949
uhj1993 <dzzd777@naver.com>
2022-08-13 23:09:03 +0900

    fix: recoil default 값 변경

--- client/src/states/loginState.ts
+++ client/src/states/loginState.ts
@@ -4,6 +4,6 @@ import { USER_TOKEN } from '@src/constants';
 
 export const isLoginState = atom({
   key: 'isLoginState',
-  default: {} as null | string,
+  default: null,
   effects: [localStorageEffect(USER_TOKEN)],
 });
