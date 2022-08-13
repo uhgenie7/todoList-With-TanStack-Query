28fb100dfe5b3e87ab58bdd56ed2e7ea76e55ad5
uhj1993 <dzzd777@naver.com>
2022-08-13 23:07:45 +0900

    refactor: token 상수로 변경

--- client/src/apis/auth/index.ts
+++ client/src/apis/auth/index.ts
@@ -2,12 +2,13 @@ import { instance } from '..';
 import type { IUserInfo } from '@src/types/userAuthTypes';
 import type { IUserAuthResponse } from '@src/types/response';
 import { AxiosResponse } from 'axios';
+import { USER_TOKEN } from '@src/constants';
 
 export const signUpAPI = async (data: IUserInfo): Promise<AxiosResponse<IUserAuthResponse>> => {
   const response = await instance.post<IUserAuthResponse>('/users/create', data);
   try {
     const { token } = response;
-    localStorage.setItem('USER_TOKEN', token);
+    localStorage.setItem(USER_TOKEN, token);
     console.log(response);
     return response;
   } catch (error: any) {
@@ -20,7 +21,7 @@ export const loginAPI = async (data: IUserInfo): Promise<AxiosResponse<IUserAuth
   try {
     const { token } = response;
     console.log(response);
-    localStorage.setItem('USER_TOKEN', token);
+    localStorage.setItem(USER_TOKEN, token);
     return response;
   } catch (error: any) {
     return error;
