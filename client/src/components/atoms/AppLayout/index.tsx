6b6b4814288945d481371a6828b582cc3d22673e
uhj1993 <dzzd777@naver.com>
2022-08-14 21:50:04 +0900

    fix: AppLayout에 Head 옮기기

--- client/src/components/atoms/AppLayout/index.tsx
+++ client/src/components/atoms/AppLayout/index.tsx
@@ -1,42 +1,19 @@
-import { useEffect, useState } from 'react';
+import Head from 'next/head';
 import styled from 'styled-components';
-import { useRouter } from 'next/router';
-import useToast from '@src/hooks/useToast';
-
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
-import { USER_TOKEN } from '@src/constants';
-import { ISSERVER } from '@src/constants';
+
 interface IProps {
   children: React.ReactNode;
 }
 
 const AppLayout = ({ children }: IProps) => {
-  // const [isLoggedIn, setIsLoggedIn] = useState<string | false | null>(null);
-  // const router = useRouter();
-  // const toast = useToast();
-  // const currentPageName = router.pathname;
-  // const pageRootName = currentPageName.slice(1).split('/')[0];
-  // const protectedPages = ['', '/', '[id]'];
-  // const isPublicPage = !protectedPages.includes(pageRootName);
-
-  // useEffect(() => {
-  //   setIsLoggedIn(!ISSERVER && localStorage.getItem(USER_TOKEN));
-  // }, []);
-
-  // useEffect(() => {
-  //   if (!isPublicPage && !isLoggedIn) {
-  //     toast.error('로그인 먼저 해주세요');
-  //     router.replace('/auth');
-  //   }
-  // }, [isLoggedIn, router]);
-
-  // if (!isPublicPage && !isLoggedIn) {
-  //   return <div>loading</div>;
-  // }
-
   return (
     <>
+      <Head>
+        <title>wanted-pre-onboarding-challenge-fe-1</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <ToastContainer />
       <Container>{children}</Container>
     </>
