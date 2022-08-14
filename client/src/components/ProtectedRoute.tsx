279629e91d9824fa1579264f46fb01c45147a0ee
uhj1993 <dzzd777@naver.com>
2022-08-14 21:48:36 +0900

    fix: (#16) 리다이렉트 Component

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,22 +1,22 @@
 import { useEffect } from 'react';
 import ROUTES from '@src/constants/routes';
 import { ISSERVER } from '@src/constants';
-import { isLoginState } from '@src/states/loginState';
-import { useRecoilValue } from 'recoil';
 import FullPageLoader from './FullPageLoader';
+import { useAppSelector } from '@src/hooks/useSelector';
 
 const ProtectedRoute = ({ router, children }) => {
-  const isAuthenticated = useRecoilValue(isLoginState);
+  const isAuthenticated = useAppSelector((state) => state.user.isLoggedIn);
+
   console.log(isAuthenticated);
   const publicedRoutes = [ROUTES.AUTH.LOGIN, ROUTES.AUTH.SIGNUP];
   const pathIsProtected = publicedRoutes.indexOf(router.pathname) === -1;
   console.log(pathIsProtected);
+
   useEffect(() => {
     if (!ISSERVER && !isAuthenticated && pathIsProtected) {
-      // Redirect route, you can point this to /login
       router.push(ROUTES.AUTH.LOGIN);
     }
-  }, [isAuthenticated, pathIsProtected]);
+  }, [isAuthenticated, pathIsProtected, router]);
 
   if (!ISSERVER && !isAuthenticated && pathIsProtected) {
     return <FullPageLoader />;
--- client/src/components/atoms/PageLoader/PageLoader.tsx
+++ client/src/components/atoms/PageLoader/PageLoader.tsx
@@ -1,4 +1,4 @@
-const FullPageLoader = () => {
+const PageLoader = () => {
   return (
     <div>
       <p>Loading...</p>
@@ -6,4 +6,4 @@ const FullPageLoader = () => {
   );
 };
 
-export default FullPageLoader;
+export default PageLoader;
