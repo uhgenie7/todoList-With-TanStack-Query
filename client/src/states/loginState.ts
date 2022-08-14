8f63e8bacdfed8641a1f716eab4cb716130b2dbf
uhj1993 <dzzd777@naver.com>
2022-08-14 21:34:47 +0900

    del: recoil 삭제

--- client/src/states/loginState.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil';
-import { localStorageEffect } from '@src/utils/localStorageEffect';
-import { USER_TOKEN } from '@src/constants';
-
-export const isLoginState = atom({
-  key: 'isLoginState',
-  default: null,
-  effects: [localStorageEffect(USER_TOKEN)],
-});
