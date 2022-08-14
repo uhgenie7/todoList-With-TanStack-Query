7ea8d09664d20ea18e2a45e5f25a357030fda72a
uhj1993 <dzzd777@naver.com>
2022-08-14 22:40:05 +0900

    fix: 로그인 여부에 따라 Header 문구 바꾸기

--- client/src/components/atoms/Header/index.tsx
+++ client/src/components/atoms/Header/index.tsx
@@ -1,9 +1,20 @@
-import { useUserActions } from '@src/hooks/useUserActions';
+import { useLogout } from '@src/hooks/useLogout';
 import Button from '@src/components/atoms/Button';
+import { useAppSelector } from '@src/hooks/useSelector';
 
 const Header = () => {
-  const userActions = useUserActions();
-  return <header>{<Button buttonValue="로그아웃" isCorrect={true} onClick={userActions.handleLogout} />}</header>;
+  const logoutAction = useLogout();
+  const isAuthenticated = useAppSelector((state) => state.user.isLoggedIn);
+
+  return (
+    <header>
+      {isAuthenticated ? (
+        <Button buttonValue="로그아웃" isCorrect={true} onClick={logoutAction.handleLogout} />
+      ) : (
+        <h1>어서오세요</h1>
+      )}
+    </header>
+  );
 };
 
 export default Header;
