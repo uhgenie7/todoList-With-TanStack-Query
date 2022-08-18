18fba4b3101d6dac47bb99157523010376e1831f
uhj1993 <dzzd777@naver.com>
2022-08-19 00:29:54 +0900

    del: AuthForm 위에서의 AsyncBoundary 삭제

--- client/src/components/pages/LoginPage.tsx
+++ client/src/components/pages/LoginPage.tsx
@@ -1,17 +1,12 @@
 import AuthForm from '@src/components/organism/AuthForm';
 import { useLoginQuery } from '@src/hooks/query/auth';
-import AsyncBoundary from '../boundaries/AsyncBoundary';
-import DefaultErrorFallback from '../atoms/DefaultErrorFallback';
-import PageLoader from '../atoms/PageLoader';
 
 const LoginPage = () => {
   return (
     <>
       <h2>로그인</h2>
       <div className="center">
-        <AsyncBoundary rejectedFallback={DefaultErrorFallback} pendingFallback={<PageLoader />}>
-          <AuthForm useQuery={useLoginQuery} buttonValue="로그인" />
-        </AsyncBoundary>
+        <AuthForm useQuery={useLoginQuery} buttonValue="로그인" />
       </div>
     </>
   );
--- client/src/pages/auth/signup/index.tsx
+++ client/src/pages/auth/signup/index.tsx
@@ -1,16 +1,11 @@
 import AuthForm from '@src/components/organism/AuthForm';
 import { useSignUpQuery } from '@src/hooks/query/auth';
-import AsyncBoundary from '@src/components/boundaries/AsyncBoundary';
-import DefaultErrorFallback from '@src/components/atoms/DefaultErrorFallback';
-import PageLoader from '@src/components/atoms/PageLoader';
 
 const SignUp = () => {
   return (
     <>
       <h2>회원가입</h2>
-      <AsyncBoundary rejectedFallback={DefaultErrorFallback} pendingFallback={<PageLoader />}>
-        <AuthForm useQuery={useSignUpQuery} buttonValue="회원가입" />
-      </AsyncBoundary>
+      <AuthForm useQuery={useSignUpQuery} buttonValue="회원가입" />
     </>
   );
 };
