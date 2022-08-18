16e5ad4d153e24b23b7d0581253ecfd60cf0a939
uhj1993 <dzzd777@naver.com>
2022-08-19 01:48:45 +0900

    feat: 할일 없을 때 문구 추가

--- client/src/components/molecules/TodoList.tsx
+++ client/src/components/molecules/TodoList.tsx
@@ -14,12 +14,15 @@ const TodoList = () => {
     <Container>
       <h2>할일 목록</h2>
       <ul className="messages">
-        {freshTodos &&
+        {freshTodos && freshTodos.length > 0 ? (
           [...freshTodos]
             .reverse()
             .map(({ id, title, content, createdAt, updatedAt }) => (
               <TodoItem id={id} key={id} title={title} content={content} createdAt={createdAt} updatedAt={updatedAt} />
-            ))}
+            ))
+        ) : (
+          <li>할일을 추가해주세요</li>
+        )}
       </ul>
     </Container>
   );
